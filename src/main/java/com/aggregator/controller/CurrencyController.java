feae16e57985631fb6b4f671c7f967c4a220a901
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-18 12:32:47 +0300

    get rid of wild card import

--- src/main/java/com/aggregator/controller/CurrencyController.java
+++ src/main/java/com/aggregator/controller/CurrencyController.java
@@ -1,9 +1,13 @@
 package com.aggregator.controller;
 
-
 import com.aggregator.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Map;
 
