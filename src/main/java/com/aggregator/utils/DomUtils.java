48177932dec0794f19dad9d1ff2031c8384ac486
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-18 12:43:02 +0300

    get rid of wild card import

--- src/main/java/com/aggregator/utils/DomUtils.java
+++ src/main/java/com/aggregator/utils/DomUtils.java
@@ -9,7 +9,12 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
@@ -66,7 +71,4 @@ public class DomUtils {
         }
         return null;
     }
-
-
-
 }
