ee1f931a8c7aec9d3cf64937f72fd0417f150f20
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-18 12:48:52 +0300

    get rid of static imports

--- src/main/java/com/aggregator/controller/CurrencyController.java
+++ src/main/java/com/aggregator/controller/CurrencyController.java
@@ -1,6 +1,8 @@
 package com.aggregator.controller;
 
+import com.aggregator.response.JsonResponse;
 import com.aggregator.service.CurrencyService;
+import com.aggregator.utils.SortMapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +13,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Map;
 
-import static com.aggregator.response.JsonResponse.getJsonFromMap;
-import static com.aggregator.response.JsonResponse.okResponse;
-import static com.aggregator.utils.SortMapUtils.sortAsc;
-import static com.aggregator.utils.SortMapUtils.sortDesc;
-
 @RestController
 public class CurrencyController {
     private final CurrencyService service;
-
 
     @Autowired
     public CurrencyController(CurrencyService service) {
@@ -28,12 +24,12 @@ public class CurrencyController {
 
     @GetMapping(value = {"", "/"}, produces = "application/json")
     public String getRates() {
-        return getJsonFromMap(service.getAllRates());
+        return JsonResponse.getJsonFromMap(service.getAllRates());
     }
 
     @GetMapping(value = "/{code}", produces = "application/json")
     public String getRatesForCode(@PathVariable(value = "code") String code) {
-        return getJsonFromMap(service.getRatesForCode(code));
+        return JsonResponse.getJsonFromMap(service.getRatesForCode(code));
     }
 
     @GetMapping(value = "/{code}/{tag}", produces = "application/json")
@@ -46,7 +42,7 @@ public class CurrencyController {
         else if (tag.equals("sell"))
             resultMap = service.getSellPricesForCode(code);
         resultMap = sortIfNeeded(sort, resultMap);
-        return getJsonFromMap(resultMap);
+        return JsonResponse.getJsonFromMap(resultMap);
     }
 
     @PutMapping(value = "/{code}/{tag}", produces = "application/json")
@@ -58,26 +54,26 @@ public class CurrencyController {
             service.updateBuyPriceForBank(bank, code, value);
         else if (tag.equals("sell"))
             service.updateSellPriceForBank(bank, code, value);
-        return okResponse;
+        return JsonResponse.okResponse;
 
     }
 
     @DeleteMapping(value = "/", produces = "application/json")
     public String deleteRatesForBank(@RequestParam("bank") String bank) {
         service.deleteRatesForBank(bank);
-        return okResponse;
+        return JsonResponse.okResponse;
     }
 
     @GetMapping(value = "/report", produces = "application/json")
     public String report() {
-        return getJsonFromMap(service.getBestPropositions());
+        return JsonResponse.getJsonFromMap(service.getBestPropositions());
     }
 
     private Map<String, Double> sortIfNeeded(@RequestParam(value = "sort", required = false) String sort, Map<String, Double> resultMap) {
         if (sort != null && sort.equals("asc")) {
-            resultMap = sortAsc(resultMap);
+            resultMap = SortMapUtils.sortAsc(resultMap);
         } else if (sort != null && sort.equals("desc")) {
-            resultMap = sortDesc(resultMap);
+            resultMap = SortMapUtils.sortDesc(resultMap);
         }
         return resultMap;
     }
