d4786300a67b1d05fee1b70552cbc687617a2724
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-18 16:50:54 +0300

    add tests for file utils

---
+++ src/test/java/com/aggregator/util/FileUtilsTest.java
@@ -0,0 +1,50 @@
+package com.aggregator.util;
+
+import com.aggregator.utils.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class FileUtilsTest {
+
+    @Test
+    public void testStripExtension(){
+        assertEquals("file", FileUtils.stripExtension("file.xml"));
+    }
+
+    @Test
+    public void testStripExtensionIfNull(){
+        assertNull(FileUtils.stripExtension(null));
+    }
+
+    @Test
+    public void testStripExtensionIfNoExtention(){
+        assertEquals("file",FileUtils.stripExtension("file"));
+    }
+
+    @Test
+    public void testGetExtension() throws IOException {
+        File file = File.createTempFile("temp", ".csv");
+        assertEquals("csv", FileUtils.getExtension(file));
+        file.delete();
+    }
+
+    @Test
+    public void testGetExtensionIfLongPath() throws IOException {
+        File file = File.createTempFile("temp", "folder2.folder1.csv");
+        assertEquals("csv", FileUtils.getExtension(file));
+        file.delete();
+    }
+
+  /*  @Test(expected = FileNotFoundException.class)
+    public void deleteIfNotExist() throws IOException {
+        File file = File.createTempFile("temp","txt");
+        file.delete();
+        FileUtils.deleteContentOfFile(file);
+    }*/
+}
