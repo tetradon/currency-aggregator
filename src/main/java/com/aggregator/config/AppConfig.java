bd14e12df318e3c60c691a3e2b6edc00202b7c6a
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-20 13:58:09 +0300

    delete unused import

--- src/main/java/com/aggregator/config/AppConfig.java
+++ src/main/java/com/aggregator/config/AppConfig.java
@@ -2,7 +2,6 @@ package com.aggregator.config;
 
 
 import com.aggregator.service.CurrencyInMemoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
