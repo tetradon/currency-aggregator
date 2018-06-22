ec6c850a92dacb2b60dab05d0afafa142e3ab140
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-22 15:54:26 +0300

    add logging for json response exceptions

--- src/main/java/com/aggregator/response/JsonResponse.java
+++ src/main/java/com/aggregator/response/JsonResponse.java
@@ -2,6 +2,8 @@ package com.aggregator.response;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 import java.util.Map;
@@ -9,6 +11,9 @@ import java.util.Map;
 public final class JsonResponse {
     public static final  String OK_RESPONSE = "{\"status\" : \"ok\"}";
     private static ObjectMapper mapper = new ObjectMapper();
+
+    private static final Logger log =
+            LogManager.getLogger(JsonResponse.class);
 
     private JsonResponse() {
     }
@@ -18,7 +23,7 @@ public final class JsonResponse {
         try {
             response = mapper.writeValueAsString(map);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            log.error("Exception while JSON processing", e);
         }
         return response;
     }
