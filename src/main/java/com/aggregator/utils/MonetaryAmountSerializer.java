a73a12d7e7550b3bf5b291ec484db567313675f7
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-26 15:55:49 +0300

    add custom serializer for MonetaryAmount

---
+++ src/main/java/com/aggregator/utils/MonetaryAmountSerializer.java
@@ -0,0 +1,29 @@
+package com.aggregator.utils;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import javax.money.MonetaryAmount;
+import java.io.IOException;
+
+public class MonetaryAmountSerializer extends StdSerializer<MonetaryAmount> {
+
+    public MonetaryAmountSerializer() {
+        this(null);
+    }
+
+    public MonetaryAmountSerializer(Class<MonetaryAmount> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(MonetaryAmount monetaryAmount,
+                          JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider)
+            throws IOException {
+        jsonGenerator.writeNumber(monetaryAmount
+                .getNumber().doubleValueExact());
+    }
+}
+
