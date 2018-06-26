f80c97b72fdd55f577d602ac0f95926252554ac7
artur.kotlyar <artur.kotlyar@gmail.com>
2018-06-26 15:58:34 +0300

    register custom serializer for MonetaryAmount

--- src/main/java/com/aggregator/response/JsonResponse.java
+++ src/main/java/com/aggregator/response/JsonResponse.java
@@ -1,11 +1,14 @@
 package com.aggregator.response;
 
+import com.aggregator.utils.MonetaryAmountSerializer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
+import javax.money.MonetaryAmount;
 import java.util.Map;
 
 public final class JsonResponse {
@@ -19,6 +22,11 @@ public final class JsonResponse {
     }
 
     public static String getJsonFromMap(final Map map) {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(MonetaryAmount.class,
+                new MonetaryAmountSerializer());
+        mapper.registerModule(module);
+
         String response = "";
         try {
             response = mapper.writeValueAsString(map);
