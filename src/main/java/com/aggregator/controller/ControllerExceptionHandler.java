2a9631bc2669ff8cf06e06a4e507ccd63704ff4d
artur.kotlyar <artur.kotlyar@gmail.com>
2018-07-04 11:03:49 +0300

    handle exception and return a message as json

--- src/main/java/com/aggregator/controller/ControllerExceptionHandler.java
+++ src/main/java/com/aggregator/controller/ControllerExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.aggregator.controller;
 
 import com.aggregator.exception.BankNotFoundException;
+import com.aggregator.exception.OperationNotSupportedException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -12,14 +15,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ControllerExceptionHandler
         extends ResponseEntityExceptionHandler {
+    private static ObjectMapper mapper = new ObjectMapper();
     private static final Logger log =
             LogManager.getLogger(ControllerExceptionHandler.class);
 
-        @ExceptionHandler({ BankNotFoundException.class })
-        public ResponseEntity<String> handleBankNotFoundException(
+        @ExceptionHandler({ BankNotFoundException.class,
+                OperationNotSupportedException.class })
+        public ResponseEntity<ObjectNode> handleUnprocessableEntity(
                 Exception e) {
             log.error(e.getMessage(), e);
+
+            ObjectNode message = mapper.createObjectNode();
+            message.put("message", e.getMessage());
             return new ResponseEntity<>(
-                    e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+                    message, HttpStatus.UNPROCESSABLE_ENTITY);
         }
 }
