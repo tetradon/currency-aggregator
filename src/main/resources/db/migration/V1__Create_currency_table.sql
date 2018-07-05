af593dbe1f7b64e0357fb9f6d47f3a1a48106eeb
artur.kotlyar <artur.kotlyar@gmail.com>
2018-07-05 18:20:39 +0300

    change decimal to double in db schema

--- src/main/resources/db/migration/V1__Create_currency_table.sql
+++ src/main/resources/db/migration/V1__Create_currency_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS currency_rates(
     bank varchar(127) not null,
     code varchar(3) not null,
-    buy decimal not null,
-    sell decimal not null,
+    buy double not null,
+    sell double not null,
     primary key(bank,code)
 );
