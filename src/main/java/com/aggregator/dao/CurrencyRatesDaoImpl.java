700c30ec628fc84933c1dde5f70598cfd63ac694
artur.kotlyar <artur.kotlyar@gmail.com>
2018-07-06 15:13:16 +0300

    add profile annotation for dao class and refactor it

--- src/main/java/com/aggregator/dao/CurrencyRatesDaoImpl.java
+++ src/main/java/com/aggregator/dao/CurrencyRatesDaoImpl.java
@@ -1,26 +1,28 @@
 package com.aggregator.dao;
 
+import com.aggregator.exception.BankNotFoundException;
+import com.aggregator.exception.CurrencyNotFoundException;
 import com.aggregator.model.CurrencyRate;
-import com.aggregator.utils.ListUtils;
+import com.aggregator.utils.BestPropositionsUtils;
 import org.javamoney.moneta.Money;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.money.MonetaryAmount;
 import javax.sql.DataSource;
-import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Component
+@Profile("db")
 public final class CurrencyRatesDaoImpl implements CurrencyRatesDao {
 
     private JdbcTemplate jdbcTemplate;
+    private static final String SELECT_ALL = "SELECT * FROM currency_rates";
 
     @Autowired
     public CurrencyRatesDaoImpl(DataSource dataSource) {
@@ -31,8 +33,7 @@ public final class CurrencyRatesDaoImpl implements CurrencyRatesDao {
     public Map<String, List<CurrencyRate>> getAllRates() {
 
         Map<String, List<CurrencyRate>> resultMap = new HashMap<>();
-        String sql = "SELECT * FROM currency_rates";
-        List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(SELECT_ALL);
 
         String previousBank = (String) rows.get(0).get("bank");
         resultMap.put(previousBank, new ArrayList<>());
@@ -83,15 +84,18 @@ public final class CurrencyRatesDaoImpl implements CurrencyRatesDao {
         return getPricesForCode(code, "sell");
     }
 
-    private Map<String, MonetaryAmount> getPricesForCode(String code, String operation) {
-        String sql = "SELECT bank, " + operation + " FROM currency_rates WHERE code = ?";
+    private Map<String, MonetaryAmount> getPricesForCode(String code,
+                                                         String operation) {
+        String sql = "SELECT bank,${column} FROM currency_rates WHERE code = ?"
+                .replace("${column}", operation);
         return jdbcTemplate.query(
                 sql,
                 rs -> {
                     Map<String, MonetaryAmount> innerMap = new HashMap<>();
                     while (rs.next()) {
                         String bank = rs.getString("bank");
-                        MonetaryAmount money = Money.of(rs.getDouble(operation), code);
+                        MonetaryAmount money =
+                                Money.of(rs.getDouble(operation), code);
                         innerMap.put(bank, money);
                     }
                     return innerMap;
@@ -100,85 +104,62 @@ public final class CurrencyRatesDaoImpl implements CurrencyRatesDao {
     }
 
     @Override
-    public void updateSellPriceForBank(String bank, String code, String value) {
-        String sql = "UPDATE currency_rates SET sell = ? WHERE bank = ? AND code = ?";
+    public void updateSellPriceForBank(String bank,
+                                       String code, String value) {
+        String sql = "UPDATE currency_rates SET sell = ? "
+                + "WHERE bank = ? AND code = ?";
+
+        throwExceptionIfWrongCode(code);
+        throwExceptionIfWrongBank(bank);
+
         jdbcTemplate.update(sql, value, bank, code);
+
+    }
+
+    private void throwExceptionIfWrongBank(String bank) {
+
+        List<Map<String, Object>> checkBank
+                = jdbcTemplate.queryForList(SELECT_ALL
+                + " WHERE bank = ?", bank);
+        if (checkBank.isEmpty()) {
+            throw new BankNotFoundException(bank);
+        }
+    }
+
+    private void throwExceptionIfWrongCode(String code) {
+        List<Map<String, Object>> checkCode
+                = jdbcTemplate.queryForList(SELECT_ALL
+                + " WHERE code = ?", code);
+        if (checkCode.isEmpty()) {
+            throw new CurrencyNotFoundException(code);
+        }
     }
 
     @Override
-    public void updateBuyPriceForBank(String bank, String code, String value) {
-        String sql = "UPDATE currency_rates SET buy = ? WHERE bank = ? AND code = ?";
+    public void updateBuyPriceForBank(String bank,
+                                      String code, String value) {
+        String sql = "UPDATE currency_rates SET buy = ? "
+                + "WHERE bank = ? AND code = ?";
+
+        throwExceptionIfWrongCode(code);
+        throwExceptionIfWrongBank(bank);
+
         jdbcTemplate.update(sql, value, bank, code);
     }
 
     @Override
     public void deleteRatesForBank(String bank) {
         String sql = "DELETE FROM currency_rates WHERE bank = ?";
+
+        throwExceptionIfWrongBank(bank);
+
         jdbcTemplate.update(sql, bank);
     }
 
     @Override
-    public Map<String, Map<String, Map.Entry<String, MonetaryAmount>>> getBestPropositions() {
-        Map<String, Map<String, Map.Entry<String, MonetaryAmount>>> result
-                = new HashMap<>();
-        Set<String> codes = getAllCodes();
-
-        for (String code : codes) {
-            MonetaryAmount maxBuy = Money.of(Double.MIN_VALUE, code);
-            String maxBuyBank = "";
-            MonetaryAmount minSell = Money.of(Double.MAX_VALUE, code);
-            String minSellBank = "";
-            for (Map.Entry<String, List<CurrencyRate>> entry
-                    : getAllRates().entrySet()) {
-
-                List<CurrencyRate> rates = ListUtils
-                        .filterOutByCode(code, entry);
-
-                if (rates.isEmpty()) {
-                    continue;
-                }
-
-                List<CurrencyRate> sortedBuyRates =
-                        ListUtils.sortByBuyPrice(rates);
-                List<CurrencyRate> sortedSellRates =
-                        ListUtils.sortBySellPrice(rates);
-                MonetaryAmount localMaxBuy =
-                        ListUtils.getLast(sortedBuyRates);
-                MonetaryAmount localMinSell =
-                        ListUtils.getFirst(sortedSellRates);
-
-                if (localMaxBuy.isGreaterThan(maxBuy)) {
-                    maxBuy = localMaxBuy;
-                    maxBuyBank = entry.getKey();
-                }
-
-                if (localMinSell.isLessThan(minSell)) {
-                    minSell = localMinSell;
-                    minSellBank = entry.getKey();
-                }
-            }
-
-            Map.Entry<String, MonetaryAmount> bankBuyEntry =
-                    new AbstractMap.SimpleEntry<>(maxBuyBank, maxBuy);
-            Map.Entry<String, MonetaryAmount> bankSellEntry =
-                    new AbstractMap.SimpleEntry<>(minSellBank, minSell);
-            Map<String, Map.Entry<String, MonetaryAmount>> mapEntry
-                    = new HashMap<>();
-            mapEntry.put("buy", bankBuyEntry);
-            mapEntry.put("sell", bankSellEntry);
-            result.put(code, mapEntry);
-        }
-        return result;
-    }
-
-    private Set<String> getAllCodes() {
-        Set<String> list = new HashSet<>();
-        for (Map.Entry<String, List<CurrencyRate>> entry
-                : getAllRates().entrySet()) {
-            for (CurrencyRate rate : entry.getValue()) {
-                list.add(rate.getCurrencyRateCode());
-            }
-        }
-        return list;
+    public Map<String,
+            Map<String,
+                    Map.Entry<String, MonetaryAmount>>> getBestPropositions() {
+        return BestPropositionsUtils.get(getAllRates());
     }
 }
