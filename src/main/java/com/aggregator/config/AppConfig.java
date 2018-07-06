8d1b1d77958c9a825d3dd81322f5e4d101507b26
artur.kotlyar <artur.kotlyar@gmail.com>
2018-07-06 12:33:06 +0300

    read property file and create appropriate beans in config class

--- src/main/java/com/aggregator/config/AppConfig.java
+++ src/main/java/com/aggregator/config/AppConfig.java
@@ -1,12 +1,19 @@
 package com.aggregator.config;
 
 
+import com.aggregator.dao.CurrencyRatesDaoImpl;
+import com.aggregator.service.CurrencyDbService;
 import com.aggregator.service.CurrencyInMemoryService;
+import com.aggregator.service.CurrencyService;
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -16,28 +23,46 @@ import javax.servlet.ServletContext;
 import javax.sql.DataSource;
 import java.io.File;
 
-
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.aggregator")
-
+@PropertySource("classpath:application.properties")
 public class AppConfig implements WebMvcConfigurer {
+    private final ConfigurableEnvironment environment;
+
+    @Autowired
+    public AppConfig(ConfigurableEnvironment env) {
+        environment = env;
+        environment.setActiveProfiles(
+                env.getProperty("spring.profiles.active"));
+    }
 
     @Bean
-    public CurrencyInMemoryService currencyInMemoryService(
+    @Profile("file")
+    public CurrencyService currencyInMemoryService(
             ServletContext servletContext) {
         File folder = new File(servletContext.getRealPath("/WEB-INF/rates/"));
         return new CurrencyInMemoryService(folder);
     }
 
+    @Bean
+    @Profile("db")
+    public CurrencyService currencyDbService() {
+        return new CurrencyDbService(
+                new CurrencyRatesDaoImpl(dataSource()));
+    }
 
     @Bean
+    @Profile("db")
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.h2.Driver");
-        dataSource.setUrl("jdbc:h2:file:~/currency_rates");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("");
+        String driver = environment.getProperty("jdbc.driver");
+        if (driver != null) {
+            dataSource.setDriverClassName(driver);
+        }
+        dataSource.setUrl(environment.getProperty("jdbc.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.password"));
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.migrate();
