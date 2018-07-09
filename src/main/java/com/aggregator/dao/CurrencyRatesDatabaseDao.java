5bf05f178785fd59cd3e0be5247ad9ae33b8ff9c
artur.kotlyar <artur.kotlyar@gmail.com>
2018-07-09 17:28:18 +0300

    get best propositions from sql query

--- src/main/java/com/aggregator/dao/CurrencyRatesDatabaseDao.java
+++ src/main/java/com/aggregator/dao/CurrencyRatesDatabaseDao.java
@@ -3,7 +3,6 @@ package com.aggregator.dao;
 import com.aggregator.exception.BankNotFoundException;
 import com.aggregator.exception.CurrencyNotFoundException;
 import com.aggregator.model.CurrencyRate;
-import com.aggregator.utils.BestPropositionsUtils;
 import org.javamoney.moneta.Money;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import javax.money.MonetaryAmount;
 import javax.sql.DataSource;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -107,7 +107,7 @@ public final class CurrencyRatesDatabaseDao implements CurrencyRatesDao {
     public void updateSellPriceForBank(String bank,
                                        String code, String value) {
         String sql = "UPDATE currency_rates SET sell = ? "
-                + "WHERE bank = ? AND code = ?";
+                     + "WHERE bank = ? AND code = ?";
 
         throwExceptionIfWrongCode(code);
         throwExceptionIfWrongBank(bank);
@@ -120,7 +120,7 @@ public final class CurrencyRatesDatabaseDao implements CurrencyRatesDao {
 
         List<Map<String, Object>> checkBank
                 = jdbcTemplate.queryForList(SELECT_ALL
-                + " WHERE bank = ?", bank);
+                                            + " WHERE bank = ?", bank);
         if (checkBank.isEmpty()) {
             throw new BankNotFoundException(bank);
         }
@@ -129,7 +129,7 @@ public final class CurrencyRatesDatabaseDao implements CurrencyRatesDao {
     private void throwExceptionIfWrongCode(String code) {
         List<Map<String, Object>> checkCode
                 = jdbcTemplate.queryForList(SELECT_ALL
-                + " WHERE code = ?", code);
+                                            + " WHERE code = ?", code);
         if (checkCode.isEmpty()) {
             throw new CurrencyNotFoundException(code);
         }
@@ -139,7 +139,7 @@ public final class CurrencyRatesDatabaseDao implements CurrencyRatesDao {
     public void updateBuyPriceForBank(String bank,
                                       String code, String value) {
         String sql = "UPDATE currency_rates SET buy = ? "
-                + "WHERE bank = ? AND code = ?";
+                     + "WHERE bank = ? AND code = ?";
 
         throwExceptionIfWrongCode(code);
         throwExceptionIfWrongBank(bank);
@@ -160,6 +160,77 @@ public final class CurrencyRatesDatabaseDao implements CurrencyRatesDao {
     public Map<String,
             Map<String,
                     Map.Entry<String, MonetaryAmount>>> getBestPropositions() {
-        return BestPropositionsUtils.get(getAllRates());
+        String sql = ""
+                     + "SELECT\n"
+                     + "  max_buy_result.code,\n"
+                     + "  max_buy_result.bank  AS max_buy_bank,\n"
+                     + "  max_buy,\n"
+                     + "  min_sell_result.bank AS min_sell_bank,\n"
+                     + "  min_sell\n"
+                     + "FROM (SELECT\n"
+                     + "        max_buy_select.code,\n"
+                     + "        bank,\n"
+                     + "        max_buy\n"
+                     + "      FROM currency_rates AS all_banks\n"
+                     + "        INNER JOIN (\n"
+                     + "                     SELECT\n"
+                     + "                       code,\n"
+                     + "                       MAX(buy) AS max_buy\n"
+                     + "                     FROM currency_rates\n"
+                     + "                     GROUP BY code\n"
+                     + "                   ) max_buy_select\n"
+                     + "          ON all_banks.code = max_buy_select.code\n"
+                     + "             AND all_banks.buy = max_buy)"
+                     + "                AS max_buy_result\n"
+                     + "  INNER JOIN (\n"
+                     + "               SELECT\n"
+                     + "                 min_sell_select.code,\n"
+                     + "                 bank,\n"
+                     + "                 min_sell\n"
+                     + "               FROM currency_rates AS all_banks\n"
+                     + "                 INNER JOIN (\n"
+                     + "                              SELECT\n"
+                     + "                                code,\n"
+                     + "                                MIN(sell) AS min_sell\n"
+                     + "                              FROM currency_rates\n"
+                     + "                              GROUP BY code\n"
+                     + "                            ) AS min_sell_select\n"
+                     + "                 ON all_banks.code "
+                     + "                        = min_sell_select.code\n"
+                     + "                      AND all_banks.sell = min_sell\n"
+                     + "             ) min_sell_result\n"
+                     + "    ON max_buy_result.code = min_sell_result.code\n";
+
+        Map<String, Map<String, Map.Entry<String, MonetaryAmount>>> result
+                = new HashMap<>();
+
+        return jdbcTemplate.query(
+                sql,
+                rs -> {
+                    while (rs.next()) {
+                        Map<String, Map.Entry<String, MonetaryAmount>> mapEntry
+                                = new HashMap<>();
+                        String code = rs.getString("code");
+
+                        String maxBuyBank = rs.getString("max_buy_bank");
+                        MonetaryAmount maxBuy =
+                                Money.of(rs.getDouble("max_buy"), code);
+                        Map.Entry<String, MonetaryAmount> bankBuyEntry =
+                                new AbstractMap
+                                        .SimpleEntry<>(maxBuyBank, maxBuy);
+                        mapEntry.put("buy", bankBuyEntry);
+
+                        String minSellBank = rs.getString("min_sell_bank");
+                        MonetaryAmount minSell =
+                                Money.of(rs.getDouble("min_sell"), code);
+                        Map.Entry<String, MonetaryAmount> bankSellEntry =
+                                new AbstractMap
+                                        .SimpleEntry<>(minSellBank, minSell);
+                        mapEntry.put("sell", bankSellEntry);
+
+                        result.put(code, mapEntry);
+                    }
+                    return result;
+                });
     }
 }
